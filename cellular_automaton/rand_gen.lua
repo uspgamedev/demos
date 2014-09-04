92b6cedef21a3e7d3f5037288a3bfe7e94f2f2d5
Renato Lui Geh <renatogeh@gmail.com>
2014-09-04 14:41:43 -0300

    Committing for later.

--- cellular_automaton/rand_gen.lua
+++ cellular_automaton/rand_gen.lua
@@ -1,1 +1,17 @@
+local generator = {
+	seed = 0
+}
+
 local RandomGenerator = {}
+
+function RandomGenerator.new(seed)
+	local inst = setmetatable({}, {__index = generator})
+
+	if not seed then 
+		inst.seed = math.random()
+	else
+		inst.seed = seed
+	end
+
+	return inst
+end
