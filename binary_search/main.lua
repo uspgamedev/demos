bf281398a7b06cbe7f7158476163551121d375ca
Kazuo256 <kazuo256@gmail.com>
2015-07-28 11:53:45 -0300

    [binsearch] Try counter

--- binary_search/main.lua
+++ binary_search/main.lua
@@ -10,6 +10,7 @@ local buttons
 local current
 local searcher
 local P, Q
+local tries
 
 local function makeButton (x, y, text, color)
   return {
@@ -39,6 +40,7 @@ local function binsearch ()
       else
         p, q = p, m
       end
+      tries = tries + 1
     end
   end)
 end
@@ -52,12 +54,13 @@ function love.load ()
   bigfont = graphics.newFont(42)
   biggerfont = graphics.newFont(64)
   P, Q = 1, 1000
+  tries = 1
   current = searcher(P, Q)
   -- UI
   buttons = {
     makeButton(W/2, 4*H/6, "YES", {100, 255, 100, 255}),
-    makeButton(W/3, 5*H/6, "SMALLER", {255, 255, 255, 255}),
-    makeButton(2*W/3, 5*H/6, "GREATER", {255, 255, 255, 255}),
+    makeButton(W/3, 5*H/6, "SMALLER", {200, 0, 0, 255}),
+    makeButton(2*W/3, 5*H/6, "GREATER", {200, 0, 0, 255}),
   }
   -- Graphics setup
   graphics.setPointStyle 'smooth'
@@ -69,19 +72,18 @@ function love.keypressed (key)
     current = searcher(false)
   elseif key == 'right' then
     current = searcher(true)
+  elseif key == 'escape' then
+    love.load()
   end
 end
 
 function love.mousepressed (x, y, button)
-  for _,button in ipairs(buttons) do
-    local d2 = (x - button.x)^2 + (y - button.y)^2
-    if d2 < 100*100 then
-      if button.text == 'SMALLER' then
-        current = searcher(false)
-      elseif button.text == 'GREATER' then
-        current = searcher(true)
-      end
-    end
+  local button = getButton(x, y)
+  if not button then return end
+  if button.text == 'SMALLER' then
+    current = searcher(false)
+  elseif button.text == 'GREATER' then
+    current = searcher(true)
   end
 end
 
@@ -106,7 +108,14 @@ function love.draw ()
   graphics.printf(P, W/8 - 16, 64 + 12, 32, 'center')
   graphics.point(7*W/8, 64)
   graphics.printf(Q, 7*W/8 - 16, 64 + 12, 32, 'center')
+  graphics.setColor(200, 0, 0, 255)
+  graphics.point(W/8 + (6*W/8)*(current/(Q - P)), 64)
+  -- Tries counter
+  graphics.setColor(200, 200, 0, 255)
+  graphics.setFont(smallfont)
+  graphics.print("#"..tries, W/8, H/4)
   -- Big number
+  graphics.setColor(255, 255, 255, 255)
   graphics.setFont(biggerfont)
   graphics.printf(current.."?", W/2 - 100, H/3, 200, 'center')
   -- Actions
